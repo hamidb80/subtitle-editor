6c8ba6d6359c039ada78e50d1e754bc38606d14d
hamidb80 <hr.bolouri@gmail.com>
2024-02-26 22:23:15 +0330

    u

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { HashRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import { Intro, Studio, Help } from "./pages"
 import { Toast } from "./components/form"
